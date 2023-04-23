95bfccb8c12b18a2e75f92f2daf2010b214adf1e
Code <Code@code.com>
2023-04-23 12:45:04 +0530

    this is first commit

---
+++ Project/ColorMix.java
@@ -0,0 +1,11 @@
+package ColorMix.Project;
+
+public class ColorMix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a color mix project");
+
+	}
+
+}
